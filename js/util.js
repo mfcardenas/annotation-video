3bde1812735842bd6b16fac1e9fb5db521d4faaf
mcardenas <marlonca@ucm.es>
2016-11-24 19:50:46 +0100

    add form google

--- js/util.js
+++ js/util.js
@@ -176,3 +176,21 @@ function getDefaultComments(){
 
     return def_comment;
 }
+
+
+/**
+ * Function isNumeric for validate input field numeric.
+ * @param elem
+ * @param helperMsg
+ * @returns {boolean}
+ */
+function isNumeric(elem, helperMsg){
+    var numericExpression = /^[0-9]+$/;
+    if(elem.value.match(numericExpression)){
+        return true;
+    }else{
+        alert(helperMsg);
+        elem.focus();
+        return false;
+    }
+}
